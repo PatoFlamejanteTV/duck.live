f269207988b9481f1fe7e69c47997af161abb6a1
UltimateQuack <patoflamejantetv@gmail.com>
2024-11-29 15:23:31 +0000

    removed basically everything about the original parrot.live
    added:
    small changes on code
    
    modified:
    gif
    
    removed:
    change color code (make the gif "ugly" + epilepsy)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const http = require('http');
 const url = require('url');
 const { Readable } = require('stream');
-const colors = require('colors/safe');
+//const colors = require('colors/safe');
 
 // Setup frames in memory
 let original;
@@ -29,7 +29,7 @@ let flipped;
   console.log(err);
 });
 
-const colorsOptions = [
+/*const colorsOptions = [
   'red',
   'yellow',
   'green',
@@ -37,17 +37,17 @@ const colorsOptions = [
   'magenta',
   'cyan',
   'white'
-];
-const numColors = colorsOptions.length;
-const selectColor = previousColor => {
-  let color;
+];*/
+//const numColors = colorsOptions.length;
+//const selectColor = previousColor => {
+  //let color;
 
-  do {
+  /*do {
     color = Math.floor(Math.random() * numColors);
   } while (color === previousColor);
 
   return color;
-};
+};*/
 
 const streamer = (stream, opts) => {
   let index = 0;
@@ -59,12 +59,12 @@ const streamer = (stream, opts) => {
     // clear the screen
     stream.push('\033[2J\033[3J\033[H');
 
-    const newColor = lastColor = selectColor(lastColor);
+    //const newColor = lastColor = selectColor(lastColor);
 
-    stream.push(colors[colorsOptions[newColor]](frames[index]));
+    stream.push((frames[index]));
 
     index = (index + 1) % frames.length;
-  }, 70);
+  }, 100);
 };
 
 const validateQuery = ({ flip }) => ({
@@ -82,7 +82,7 @@ const server = http.createServer((req, res) => {
     req.headers['user-agent'] &&
     !req.headers['user-agent'].includes('curl')
   ) {
-    res.writeHead(302, { Location: 'https://github.com/hugomd/parrot.live' });
+    res.writeHead(302, { Location: 'https://github.com/PatoFlamejanteTV/duck.live' });
     return res.end();
   }
 
